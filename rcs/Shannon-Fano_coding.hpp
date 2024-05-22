020b0d5cc1ae11b918f3a3d26837c410cea213b7
BrainR <constantin.m.petroo@gmail.com>
2024-05-22 20:28:38 +0300

    Added to the project and added them namespaces BinSTree Queue QueueArray

--- rcs/Shannon-Fano_coding.hpp
+++ rcs/Shannon-Fano_coding.hpp
@@ -1,9 +1,26 @@
-#ifndef SHANNON_FANO_CODING
-#define SHANNON_FANO_CODING
+#ifndef SHANNON_FANO_CODING_HPP
+#define SHANNON_FANO_CODING_HPP
+
+#include <string>
+#include <vector>
 
 namespace sfc
 {
+  class SFCoding
+  {
+    public:
+    SFCoding();
+    SFCoding(std::vector<std::string> files);
+    SFCoding(const SFCoding& src) = delete;
+    SFCoding(const SFCoding&& src) = delete;
 
+    SFCoding& operator=(const SFCoding& src) = delete;
+    SFCoding& operator=(const SFCoding&& src) = delete;
+
+    private:
+    std::vector<std::string> files;
+
+  };
 }
 
 #endif
