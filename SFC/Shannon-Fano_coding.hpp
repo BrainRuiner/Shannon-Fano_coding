d9302884624086c876ee6e88b7e75da7f0a16348
BRuiner <bruiner@DebianBR.DebianBR>
2024-07-03 00:54:43 +0300

    Forgot to save small changes in SFC

--- SFC/Shannon-Fano_coding.hpp
+++ SFC/Shannon-Fano_coding.hpp
@@ -7,15 +7,25 @@
 
 namespace sfc
 {
-  struct SFCoding
+  class SFCoding
   {
+    public:
+    SFCoding();
+    SFCoding(const SFCoding&) = delete;
+    SFCoding(SFCoding&&) = delete;
+    ~SFCoding();
+
+    SFCoding& operator=(const SFCoding&) = delete;
+    SFCoding& operator=(SFCoding&&) = delete;
+
     void readCommands(std::istream& in, std::ostream& out);
     void decode(std::istream& in);
     void encode(std::istream& in);
     void autoCodes(std::istream& in);
 
     private:
-    utl::Node* codes_ = nullptr;
+
+    utl::Node* codes_;
     size_t size_ = 0;
 
     char decode(const std::string& src);
