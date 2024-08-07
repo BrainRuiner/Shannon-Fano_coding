df51dcbd2165cc3396d0d490a414f26d34571fa4
BrainR <constantin.m.petroo@gmail.com>
2024-08-07 23:03:37 +0300

    Books tests added

--- main.cpp
+++ main.cpp
@@ -20,8 +20,38 @@ void bigTest(){
   std::cout << "decode: " << std::clock() - c1 << '\n';
 }
 
+void bookTest(){
+  auto c1 = std::clock();
+  fileWork::makeCodes(std::cout, "alice.txt", "aliceCodes.txt");
+  std::cout << "makeCodes: " << std::clock() - c1 << '\n';
+  c1 = std::clock();
+  fileWork::encode(std::cout, "alice.txt", "aliceBinary.bin",
+    "aliceCodes.txt");
+  std::cout << "encode: " << std::clock() - c1 << '\n';
+  c1 = std::clock();
+  fileWork::decode(std::cout, "aliceBinary.bin", "aliceCodes.txt",
+    "aliceRes.txt");
+  std::cout << "decode: " << std::clock() - c1 << '\n';
+}
+
+void bookTest2(){
+  auto c1 = std::clock();
+  fileWork::makeCodes(std::cout, "mobyDick.txt", "mobyDickCodes.txt");
+  std::cout << "makeCodes: " << std::clock() - c1 << '\n';
+  c1 = std::clock();
+  fileWork::encode(std::cout, "mobyDick.txt", "mobyDickBinary.bin",
+    "mobyDickCodes.txt");
+  std::cout << "encode: " << std::clock() - c1 << '\n';
+  c1 = std::clock();
+  fileWork::decode(std::cout, "mobyDickBinary.bin", "mobyDickCodes.txt",
+    "mobyDickRes.txt");
+  std::cout << "decode: " << std::clock() - c1 << '\n';
+}
+
 int main(){
-  bigTest();
+  //bigTest();
+  bookTest();
+  bookTest2();
   // fileWork::runCommandLoop(std::cout, std::cin);
   return 0;
 }
